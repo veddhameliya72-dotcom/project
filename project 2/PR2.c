8c2e26ce79cce4b594cdec7266aeea61f6f1a372
veddhameliya72-dotcom <veddhameliya72@gmail.com>
2026-04-06 18:11:45 +0530

    Add files via upload

---
+++ project 2/PR2.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+	char ch;
+	printf("enter your marks:");
+	scanf("%d",&marks);
+	
+	marks>=90
+	?(ch = 'A',printf("Grade A/n")):
+	(marks>=80)?(ch = 'B',printf("Grade B\n")):
+	(marks>=70)?(ch = 'C',printf("Grade c\n")):
+	(marks>=35)?(ch = 'D',printf("Grade D\n")):
+	(ch = 'F',printf("Grade F\n"));
+	
+	switch(ch){
+		case 'A':printf("Excellent work");
+		break;
+		case 'B':printf("Well done");
+		break;
+		case 'C':printf("Good job");
+		break;
+		case 'D':printf("You passed");
+		break;
+		case 'F':printf("Fail");
+		break;
+     }
+
+  if(ch == 'A','B','C','D')printf("congratulations! \n you are eligible for next level.");else	printf("please try  again next time");
+}
+
+
+
+
+
+
+
+
+/*output:
+
+1.
+enter your marks:92
+Grade A
+
+2.
+enter your marks:92
+Grade A
+Excellent work
+3.
+enter your marks:95
+Grade A
+Excellent workcongratulations!
+ you are eligible for next level.
+
+
+*/
